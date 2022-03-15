96a65dc3284b8ef8b7c667c255c27d4c80857366
Jeremiah Carey <jeremiah.a.carey@gmail.com>
2022-03-14 22:28:21 -0400

    cleaned up js code

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const pixelsUp = document.querySelector('#pixels-up-btn');
 const pixelsDown = document.querySelector('#pixels-down-btn');
 const blackPaint = document.querySelector('#black-option');
 const rainbowPaint = document.querySelector('#rainbow-option');
+const paintCanvas = document.querySelector('#canvas');
 let gridBlocks
 
 
@@ -10,24 +11,12 @@ let blocksAcross = 25;
 let paintOpt = 'black'
 
 function randomColor() {
-    const r = Math.floor(Math.random() * 255 + 1);
-    const g = Math.floor(Math.random() * 255 + 1);
-    const b = Math.floor(Math.random() * 255 + 1);
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
     return `rgb(${r}, ${g}, ${b})`;
 }
 
-function startPaintBlack() {
-    gridBlocks.forEach(block => block.addEventListener('mouseover', function (e) {
-        e.target.style.backgroundColor = 'black';
-    }))
-}
-
-function startPaintRainbow() {
-    gridBlocks.forEach(block => block.addEventListener('mouseover', function (e) {
-        e.target.style.backgroundColor = randomColor();
-    }))
-}
-// function to make the grid, given a certain number of blocks across
 
 function makeGrid(blocksAcross) {
     const area = blocksAcross * blocksAcross;
@@ -37,25 +26,26 @@ function makeGrid(blocksAcross) {
         gridBlock.id = `grid-block-${i}`;
         gridBlock.style.width = `${550 / blocksAcross}px`;
         gridBlock.style.height = gridBlock.style.width;
-        document.getElementById('canvas').appendChild(gridBlock);
+        paintCanvas.appendChild(gridBlock);
         gridBlocks = document.querySelectorAll('.grid-block');
-    }
-    if (paintOpt === 'black') {
-        startPaintBlack();
-    } else {
-        startPaintRainbow();
     }
 }
 
 makeGrid(blocksAcross);
 
-// Remove the grid (needed to repopulate with new pixel density)
+paintCanvas.addEventListener('mouseover', function (e) {
+    if (paintOpt === 'black') {
+        e.target.style.backgroundColor = 'black';
+    } else {
+        e.target.style.backgroundColor = randomColor();
+    }
+})
+
 
 function removeGrid() {
     gridBlocks.forEach(block => block.remove());
 }
 
-// Clear the grid
 
 clearBtn.addEventListener('click', function () {
     gridBlocks.forEach(block => block.style.backgroundColor = '#eff3f1');
@@ -65,18 +55,14 @@ blackPaint.addEventListener('click', function () {
     paintOpt = 'black';
     blackPaint.classList.add('option-selected');
     rainbowPaint.classList.remove('option-selected');
-    startPaintBlack();
-
 })
 
 rainbowPaint.addEventListener('click', function () {
     paintOpt = 'rainbow';
     rainbowPaint.classList.add('option-selected');
     blackPaint.classList.remove('option-selected');
-    startPaintRainbow();
 })
 
-// Increase/Decrease Pixel Density
 
 pixelsUp.addEventListener('click', function () {
     if (blocksAcross < 75) {
